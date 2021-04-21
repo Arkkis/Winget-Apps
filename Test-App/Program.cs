9ca63331749417d296507a657038bfa446437fa3
Arkkis <joni.p.nieminen@protonmail.com>
2021-04-21 19:25:07 +0300

    Initial commit

---
+++ Test-App/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Winget_Apps;
+using System.IO.Compression;
+
+namespace Test_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test().GetAwaiter().GetResult();
+        }
+
+        private static async Task<string> Test()
+        {
+            var extractedZipPath = $"{AppContext.BaseDirectory}winget-pkgs-master";
+            var zipPath = $"{AppContext.BaseDirectory}master.zip";
+            var cachePath = $"{AppContext.BaseDirectory}cache";
+
+            if (Directory.Exists(extractedZipPath))
+            {
+                Directory.Delete(extractedZipPath, true);
+            }
+
+            var webClient = new WebClient();
+            webClient.Headers.Add("User-Agent", "request");
+            webClient.DownloadFile(new Uri("https://github.com/microsoft/winget-pkgs/archive/refs/heads/master.zip"), zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, AppContext.BaseDirectory);
+
+            if (!Directory.Exists(cachePath))
+            {
+                Directory.Move($"{extractedZipPath}\\manifests", cachePath);
+            }
+
+            if (Directory.Exists(extractedZipPath))
+            {
+                Directory.Delete(extractedZipPath, true);
+            }
+
+            if (File.Exists(zipPath))
+            {
+                File.Delete(zipPath);
+            }
+
+            var wingetApps = new WingetApps();
+
+            var test = await wingetApps.GetWingetApps(60 * 24);
+            var appCount = 0;
+
+            // Getting disctinct items by Id with GroupBy
+            foreach (var app in test.GroupBy(a => a.PackageId).Select(g => g.First()).ToList())
+            {
+                Console.WriteLine(app.Name);
+                appCount++;
+            }
+
+            Console.WriteLine($"Total apps: {appCount}");
+            Console.ReadKey();
+
+            return "";
+        }
+    }
+}
---
+++ Winget-Apps/WingetApps.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Winget_Apps
+{
+    public class WingetApps
+    {
+        public async Task<List<AppDto>> GetWingetApps(int cacheExpirationTimeInMinutes)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var cacheDirectory = $"{AppContext.BaseDirectory}cache\\";
+            var nameCachePath = $"{cacheDirectory}NameCache.txt";
+            var appListCachePath = $"{cacheDirectory}AppListCache.json";
+
+            if (File.Exists(appListCachePath))
+            {
+                var cachedAppList = JsonSerializer.Deserialize<AppListDto>(File.ReadAllText(appListCachePath));
+
+                if (cachedAppList.CreatedAt.AddMinutes(cacheExpirationTimeInMinutes) > DateTime.UtcNow)
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds}ms");
+                    return cachedAppList.Apps;
+                }
+            }
+
+            var packages = new Dictionary<string, string>();
+
+            var fileTreeResponse = await GetOrLoadFromCache<TreeDto>("https://api.github.com/repos/microsoft/winget-pkgs/git/trees/master", $"{cacheDirectory}tree.json");
+            var sha = fileTreeResponse.tree.FirstOrDefault(file => file.path == "manifests")?.sha;
+
+            if (string.IsNullOrEmpty(sha))
+            {
+                throw new NullReferenceException(nameof(sha));
+            }
+
+            var appsResponse = await GetOrLoadFromCache<TreeDto>($"https://api.github.com/repos/microsoft/winget-pkgs/git/trees/{sha}?recursive=1", $"{cacheDirectory}treelist.json");
+            var apps = appsResponse.tree.Where(file => file.type == "blob").ToList();
+
+            var appList = new List<AppDto>();
+
+            var monthExpireTimeInMinutes = 43200;
+
+            var result = new Dictionary<string, string>();
+
+            if (File.Exists(nameCachePath))
+            {
+                foreach (var line in File.ReadAllLines(nameCachePath))
+                {
+                    var keyValue = line.Split('=');
+
+                    if (keyValue.Length == 2)
+                    {
+                        result.Add(keyValue[0], keyValue[1]);
+                    }
+                }
+            }
+
+            foreach (var app in apps)
+            {
+                var appPath = app.path.Split("/");
+                var packageIdentifier = $"{appPath[1]}.{appPath[2]}";
+
+                if (result.ContainsKey(packageIdentifier))
+                {
+                    appList.Add(
+                            new AppDto
+                            {
+                                PackageId = packageIdentifier,
+                                Name = result[packageIdentifier]
+                            });
+                    continue;
+                }
+
+                var yamlPath = $"{cacheDirectory}{app.path.Replace("/", "\\")}";
+                var yamlResponse = await GetOrLoadFromCache<string>($"https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/{app.path}", yamlPath, monthExpireTimeInMinutes);
+
+                var stringReader = new StringReader(yamlResponse);
+                var deserializer = new Deserializer();
+                var appData = deserializer.Deserialize<dynamic>(stringReader);
+
+                try
+                {
+                    if (!string.IsNullOrEmpty(appData["PackageIdentifier"]) && !string.IsNullOrEmpty(appData["PackageName"]))
+                    {
+                        if (!result.ContainsKey(appData["PackageIdentifier"]))
+                        {
+                            result.Add(appData["PackageIdentifier"], appData["PackageName"]);
+                        }
+
+                        appList.Add(
+                            new AppDto
+                            {
+                                PackageId = appData["PackageIdentifier"],
+                                Name = appData["PackageName"]
+                            });
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                }
+            }
+
+            using var nameCacheFile = new StreamWriter(nameCachePath);
+            foreach (var entry in result)
+            {
+                nameCacheFile.WriteLine("{0}={1}", entry.Key, entry.Value);
+            }
+
+            if (File.Exists(appListCachePath))
+            {
+                File.WriteAllText(appListCachePath, "");
+            }
+
+            File.WriteAllText(appListCachePath, JsonSerializer.Serialize(new AppListDto { Apps = appList, CreatedAt = DateTime.UtcNow }));
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time elapsed: {stopwatch.ElapsedMilliseconds}ms");
+
+            return appList;
+        }
+
+        private async Task<T> GetOrLoadFromCache<T>(string url, string cacheFile, int expireTimeInMinutes = 60)
+        {
+            var response = "";
+            var cacheDirectory = Path.GetDirectoryName(cacheFile);
+            var cacheInvalidated = false;
+
+            if (!Directory.Exists(cacheDirectory))
+            {
+                Directory.CreateDirectory(cacheDirectory);
+            }
+
+            if (File.Exists(cacheFile))
+            {
+                if (typeof(T) == typeof(string))
+                {
+                    return (T)Convert.ChangeType(File.ReadAllText(cacheFile), typeof(T));
+                }
+
+                var cachedFile = JsonSerializer.Deserialize<CachedFile>(File.ReadAllText(cacheFile));
+
+                if (cachedFile.CreatedAt.AddMinutes(expireTimeInMinutes) > DateTime.UtcNow)
+                {
+                    response = Encoding.UTF8.GetString(Convert.FromBase64String(cachedFile.Content));
+                }
+                else
+                {
+                    cacheInvalidated = true;
+                }
+            }
+            else
+            {
+                cacheInvalidated = true;
+            }
+
+            if (cacheInvalidated)
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "request");
+                response = await httpClient.GetStringAsync(url);
+
+                var cachedFile = new CachedFile { CreatedAt = DateTime.UtcNow, Content = Convert.ToBase64String(Encoding.UTF8.GetBytes(response)) };
+
+                var fileDirectory = Path.GetDirectoryName(cacheFile);
+
+                if (!Directory.Exists(fileDirectory))
+                {
+                    Directory.CreateDirectory(fileDirectory);
+                }
+
+                File.WriteAllText(cacheFile, JsonSerializer.Serialize(cachedFile));
+
+                await Task.Delay(1000);
+            }
+
+            if (typeof(T) == typeof(string))
+            {
+                return (T)Convert.ChangeType(response, typeof(T));
+            }
+
+            var responseDto = JsonSerializer.Deserialize<T>(response);
+            return responseDto;
+        }
+
+        private async Task<T> GetResponse<T>(string url)
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "request");
+            var response = await httpClient.GetStringAsync(url);
+            var responseDto = JsonSerializer.Deserialize<T>(response);
+            return responseDto;
+        }
+
+        public class CachedFile
+        {
+            public DateTime CreatedAt { get; set; }
+            public string Content { get; set; }
+        }
+
+        public class AppData
+        {
+            public string download_url { get; set; }
+        }
+
+        public class AppListDto
+        {
+            public DateTime CreatedAt { get; set; }
+            public List<AppDto> Apps { get; set; }
+        }
+
+        public class AppDto
+        {
+            public string Publisher { get; set; }
+            public string Name { get; set; }
+            public string PackageId { get; set; }
+            public string Version { get; set; }
+        }
+
+        public class TreeDto
+        {
+            public List<FileDto> tree { get; set; }
+        }
+
+        public class FileDto
+        {
+            public string path { get; set; }
+            public string sha { get; set; }
+            public string type { get; set; }
+        }
+    }
+}
